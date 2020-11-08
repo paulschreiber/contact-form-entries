ea631fbe8dfa4db4cbee2364160a7e179739e695
Paul Schreiber <paulschreiber@gmail.com>
2020-11-08 15:03:02 -0500

    PHPCBF fixes

--- templates/print.php
+++ templates/print.php
@@ -1,24 +1,24 @@
 <?php
-  if ( ! defined( 'ABSPATH' ) ) {
-     exit;
- } 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <title><?php _e('CRM Perks Entries > Print Entries','contact-form-entries');   ?></title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title><?php _e( 'CRM Perks Entries > Print Entries', 'contact-form-entries' ); ?></title>
 </head>
 <body onload="window.print();">
 <style type="text/css">
-     body{
-         font-family: "Open Sans",sans-serif;
-         font-size: 14px;
-         margin: 0px;
-     }
-     .clear{
-         clear: both;
-     }
+	 body{
+		 font-family: "Open Sans",sans-serif;
+		 font-size: 14px;
+		 margin: 0px;
+	 }
+	 .clear{
+		 clear: both;
+	 }
   label span.howto { cursor: default; }
   
   .vx_required{color:red;}
@@ -27,7 +27,7 @@
   -moz-box-sizing: border-box;
   box-sizing: border-box;
   }
-    .vx_div{
+	.vx_div{
   padding: 10px 0px 0px 0px;
   }
   .vx_head{
@@ -35,15 +35,15 @@
   background: #eee;
   font-weight: bold;
   border: 1px solid #d9d9d9;
-    -moz-user-select: none;
+	-moz-user-select: none;
   -webkit-user-select: none;
   -ms-user-select: none;
   }
-    .vx_group{
-      border: 1px dashed #d0d0d0;
-      border-top-width: 0px ;
-      padding: 14px;
-        background: #fff;
+	.vx_group{
+	  border: 1px dashed #d0d0d0;
+	  border-top-width: 0px ;
+	  padding: 14px;
+		background: #fff;
   }
 
   .vx_col1{float:left; width: 25%; padding: 10px; background: #f6f6f6; border:2px solid #fff; font-weight: bold;}
@@ -88,34 +88,34 @@ margin-top: 10px;
 color: #E31230;
 }
 .vx_label{
-    font-weight: bold;
+	font-weight: bold;
 }
 .vx_error{
-    background: #ca5952;
-    padding: 10px;
-    font-size: 14px;
-    margin: 1% 2%;
-    color: #fff;
+	background: #ca5952;
+	padding: 10px;
+	font-size: 14px;
+	margin: 1% 2%;
+	color: #fff;
 }
 .crm_panel .vx_error{
-    margin: 0;
+	margin: 0;
 }
 .vx_check{
-    margin-bottom: 2px;
+	margin-bottom: 2px;
 }
 .vx_edit{
-    display: none;
+	display: none;
 }
 .vx_check_label{
 font-weight: bold;
 }
 .vx_del_link , .vx_del_link a{
-    color: #a00;
-    text-decoration: none;
+	color: #a00;
+	text-decoration: none;
 }
 .vx_del_link:hover , .vx_del_link a:hover{
-    color: #D54E21;
-    text-decoration: underline;
+	color: #D54E21;
+	text-decoration: underline;
 }
 .vx_float_right{
   float: right;  
@@ -124,7 +124,7 @@ font-weight: bold;
 /************notes***************/
 
 .vx_wrap .crm_note_text_area{
-    height: 60px; margin-bottom: 6px;
+	height: 60px; margin-bottom: 6px;
 }
 
 .vx_wrap .post_time{
@@ -135,7 +135,7 @@ float: right;
 }
 
 .vx_wrap .crm_note{
-    margin: 3px 0 16px 0;
+	margin: 3px 0 16px 0;
 }
 .vx_wrap .crm_check{
   width: 30px; padding: 10px 0 4px 2px;  
@@ -145,35 +145,35 @@ float: right;
  max-width: 60px; max-height: 60px;    
 }
 .vx_wrap .crm_arrow_left {
-    position: relative;
-    background: #FFF6E2;
-    border-left: 3px solid #E69F06;
-    padding: 10px;
-    margin:5px 0px 5px 75px;
+	position: relative;
+	background: #FFF6E2;
+	border-left: 3px solid #E69F06;
+	padding: 10px;
+	margin:5px 0px 5px 75px;
 color: #666;
 }
 .vx_wrap .crm_arrow_left:after, .vx_wrap .crm_arrow_left:before {
-    right: 100%;
-    top: 20px;
-    border: solid transparent;
-    content: " ";
-    height: 0;
-    width: 0;
-    position: absolute;
-    pointer-events: none;
+	right: 100%;
+	top: 20px;
+	border: solid transparent;
+	content: " ";
+	height: 0;
+	width: 0;
+	position: absolute;
+	pointer-events: none;
 }
 
 .vx_wrap .crm_arrow_left:after {
-    border-color: rgba(255, 255, 255, 0);
-    border-right-color: #FFF6E2;
-    border-width: 8px;
-    margin-top: -8px;
+	border-color: rgba(255, 255, 255, 0);
+	border-right-color: #FFF6E2;
+	border-width: 8px;
+	margin-top: -8px;
 }
 .vx_wrap .crm_arrow_left:before {
-    border-color: rgba(204, 204, 204, 0);
-    border-right-color: #E69F06;
-    border-width: 12px;
-    margin-top: -12px;
+	border-color: rgba(204, 204, 204, 0);
+	border-right-color: #E69F06;
+	border-width: 12px;
+	margin-top: -12px;
 }
 .vx_wrap .vx_note_red .crm_arrow_left{
   background-color: #ffede8;  
@@ -203,62 +203,62 @@ color: #666;
   border-color: #28b42d;
 }
 .vx_wrap .crm_img_div img{
-    width: 100%; height: 100%;
+	width: 100%; height: 100%;
 }
 .crm_note .key_info{
-    border-top: 1px solid #EAB84C;
-    margin-top: 12px;
-    padding-top: 7px;
+	border-top: 1px solid #EAB84C;
+	margin-top: 12px;
+	padding-top: 7px;
 }
 .right_links , .right_links a{
-    font-size: 14px;
+	font-size: 14px;
 }
 .crm_btn_div{
-    display: none;
+	display: none;
 }
 .vx_wrap .del_note{
 display: none;
 }
 .crm_head_div{
-    padding: 10px 20px;
+	padding: 10px 20px;
 }
 .reg_proc{
-    display: none;
+	display: none;
 }
 .hide_empty .reg_ok , .hide_empty .vx_empty_row{
   display: none;   
 }
 .hide_empty .reg_proc{
-    display: initial;
+	display: initial;
 }
 .vx_col2 img{
-    height: 70px;
+	height: 70px;
 }
 .vx_edit_note_btn{
-    display: none;
+	display: none;
 }
 .img_label{
-    height: 94px;
+	height: 94px;
 }
 .topbar a:hover{
-    color: #30aae1;
+	color: #30aae1;
 }
 .topbar a{
-    color: #21759B;
-    text-decoration: none;
-    display: inline;
-    font-weight: 400;
-    letter-spacing: normal;
+	color: #21759B;
+	text-decoration: none;
+	display: inline;
+	font-weight: 400;
+	letter-spacing: normal;
 }
 .topbar{
-    padding: 15px;
-    border-bottom: 2px solid #C5D7F1;
-    background-color: #DFEFFF;
-    color: #18749D;
+	padding: 15px;
+	border-bottom: 2px solid #C5D7F1;
+	background-color: #DFEFFF;
+	color: #18749D;
 }
 @media print{
   .topbar{
-      display: none;
+	  display: none;
   }
   body {-webkit-print-color-adjust: exact;}  
 }
@@ -268,118 +268,129 @@ function toggle(){
    var element = document.getElementById('body');
    if(!element.classList){ return false; }
    if(element.classList.contains('hide_empty')){
-    element.classList.remove("hide_empty");   
+	element.classList.remove("hide_empty");   
    }else{
-    element.classList.add("hide_empty");   
+	element.classList.add("hide_empty");   
    }
-    
+	
 }
 </script>
   <?php
-  if(count($leads)>0){
-  ?>
+	if ( count( $leads ) > 0 ) {
+		?>
   <div class="vx_wrap" id="body"> 
 <div class="topbar">
-<div style="font-size: 20px; float: left;"><?php _e('Print Preview','contact-form-entries') ?></div>
+<div style="font-size: 20px; float: left;"><?php _e( 'Print Preview', 'contact-form-entries' ); ?></div>
 <div style="font-size: 20px; float: right;" class="right_links" >
-<a href="javascript:;" onclick="window.print();" title="<?php _e('Print This Page','contact-form-entries') ?>"><?php _e('Print This Page','contact-form-entries') ?></a> 
-| <a href="javascript:toggle()" title="<?php _e('Toggle Empty Fields','contact-form-entries') ?>">
-<span class="reg_ok"><?php _e('Hide Empty Fields','contact-form-entries') ?></span>
-<span class="reg_proc"><?php _e('Show Empty Fields','contact-form-entries') ?></span>
+<a href="javascript:;" onclick="window.print();" title="<?php _e( 'Print This Page', 'contact-form-entries' ); ?>"><?php _e( 'Print This Page', 'contact-form-entries' ); ?></a> 
+| <a href="javascript:toggle()" title="<?php _e( 'Toggle Empty Fields', 'contact-form-entries' ); ?>">
+<span class="reg_ok"><?php _e( 'Hide Empty Fields', 'contact-form-entries' ); ?></span>
+<span class="reg_proc"><?php _e( 'Show Empty Fields', 'contact-form-entries' ); ?></span>
 </a>
-| <a href="javascript:window.close()" title="<?php _e('Close Window','contact-form-entries') ?>"><?php _e('Close Window','contact-form-entries') ?></a></div>
+| <a href="javascript:window.close()" title="<?php _e( 'Close Window', 'contact-form-entries' ); ?>"><?php _e( 'Close Window', 'contact-form-entries' ); ?></a></div>
 <div style="clear: both;"></div>
 </div>
 <div class="vx_contents">
-  <?php
-       foreach($leads as $id=>$lead){  
-     $notes=$detail=array();
-     if(isset($lead['lead'])){
-    $detail=$lead['lead'];     
-     }
-    if(isset($lead['notes'])){
-    $notes=$lead['notes'];     
-     }
-  ?>
-    <div class="vx_div">
-      <div class="vx_head">
-<div class="crm_head_div"> <?php echo sprintf(__('Entry # %d','contact-form-entries'),$id); ?></div>
+		<?php
+		foreach ( $leads as $id => $lead ) {  
+			$notes = $detail = [];
+			if ( isset( $lead['lead'] ) ) {
+				$detail = $lead['lead'];     
+			}
+			if ( isset( $lead['notes'] ) ) {
+				$notes = $lead['notes'];     
+			}
+			?>
+	<div class="vx_div">
+	  <div class="vx_head">
+<div class="crm_head_div"> <?php echo sprintf( __( 'Entry # %d', 'contact-form-entries' ), $id ); ?></div>
 <div class="crm_clear"></div> 
   </div>
   <div class="vx_group">
-  <?php
-  if(is_array($detail) && is_array($fields)){
-  foreach($fields as $field){ //var_dump($field['basetype']); //continue;
-      $lead_field=isset($detail[$field['name']]) ? $detail[$field['name']] : '';
-      $value='';
-      if(!empty($lead_field['value'])){
-         $value=maybe_unserialize($lead_field['value']);
-if(!empty($field['values'])){
- $value=vxcf_form::check_option_value($field['values'],$value);   
-}
-      }
+			<?php
+			if ( is_array( $detail ) && is_array( $fields ) ) {
+				foreach ( $fields as $field ) { // var_dump($field['basetype']); //continue;
+					$lead_field = isset( $detail[ $field['name'] ] ) ? $detail[ $field['name'] ] : '';
+					$value      = '';
+					if ( ! empty( $lead_field['value'] ) ) {
+						 $value = maybe_unserialize( $lead_field['value'] );
+						if ( ! empty( $field['values'] ) ) {
+							$value = vxcf_form::check_option_value( $field['values'], $value );   
+						}
+					}
 
-     // $id=empty($lead_field['id']) ? $field['name'] : $lead_field['id']; 
-      $f_name=$field['name']; 
-     // $type=$field['basetype'];
-      $req=strpos($field['type'],'*') !==false ? 'required="required"' : '';
+					// $id=empty($lead_field['id']) ? $field['name'] : $lead_field['id']; 
+					$f_name = $field['name']; 
+					// $type=$field['basetype'];
+					$req = strpos( $field['type'], '*' ) !== false ? 'required="required"' : '';
   
-      ?>
-  <div class="vx_row <?php if($value == ''){ echo 'vx_empty_row'; } ?>">
+					?>
+  <div class="vx_row 
+					<?php 
+					if ( $value == '' ) {
+						echo 'vx_empty_row'; } 
+					?>
+	">
   <div class="vx_col1">
   <label for="vx_<?php echo $field['name']; ?>" class="left_header"><?php echo $field['label']; ?></label>
   </div>
   <div class="vx_col2">
-  <?php
-  if($value == ''){ $value='&nbsp;'; }
-    if(is_array($value)){$value=implode(', ',$value); } 
-    if( filter_var($value, FILTER_VALIDATE_URL)){
-?><a href="<?php echo $value ?>" target="_blank"><?php echo $value ?></a><?php     
-}else if(filter_var($value, FILTER_VALIDATE_EMAIL)){
-?><a href="mailto:<?php echo $value ?>"><?php echo $value ?></a><?php 
-}else{
-  echo $value;  
-}
+					<?php
+					if ( $value == '' ) {
+						$value = '&nbsp;'; }
+					if ( is_array( $value ) ) {
+						$value = implode( ', ', $value ); } 
+					if ( filter_var( $value, FILTER_VALIDATE_URL ) ) {
+						?>
+		<a href="<?php echo $value; ?>" target="_blank"><?php echo $value; ?></a>
+						<?php     
+					} elseif ( filter_var( $value, FILTER_VALIDATE_EMAIL ) ) {
+						?>
+			<a href="mailto:<?php echo $value; ?>"><?php echo $value; ?></a>
+						 <?php 
+					} else {
+						echo $value;  
+					}
 
- ?>
+					?>
   </div>
   <div class="clear"></div>
   </div>
-  <?php
-  } }
-  ?>
+					<?php
+				} 
+			}
+			?>
   </div>
   </div>
-  <?php
-      if(count($notes)>0){
-  ?>
+			<?php
+			if ( count( $notes ) > 0 ) {
+				?>
   <div class="vx_div">
-        <div class="vx_head">
-<div class="crm_head_div"><?php _e('Notes.', 'contact-form-entries'); ?></div>
+		<div class="vx_head">
+<div class="crm_head_div"><?php _e( 'Notes.', 'contact-form-entries' ); ?></div>
 <div class="crm_clear"></div> 
   </div>
 
   <div class="vx_group">
-    <div class="crm_notes_div">
-<?php
-    foreach($notes as $note){
-$this->note_template($note);
-    } 
- ?>
+	<div class="crm_notes_div">
+				<?php
+				foreach ( $notes as $note ) {
+					$this->note_template( $note );
+				} 
+				?>
 </div>
 
   </div>
   </div>
- <?php 
-      }
-
- }
- ?>
+				<?php 
+			}
+		}
+		?>
    </div>
  </div>
- <?php
-  }
-  ?>
+		<?php
+	}
+	?>
 
   </body>
   </html>
--- uninstall.php
+++ uninstall.php
@@ -2,14 +2,14 @@
 /**
  * Uninstall
  */
- if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
 }
-$path=plugin_dir_path(__FILE__);
-include_once($path . "contact-form-entries.php");
- include_once($path . "includes/install.php");
-   $install=new vxcf_form_install();
-   $settings=get_option(vxcf_form::$id.'_meta',array());
-if(!empty($settings['plugin_data'])){
-  $install->remove_data();
+$path = plugin_dir_path( __FILE__ );
+require_once $path . 'contact-form-entries.php';
+ require_once $path . 'includes/install.php';
+   $install  = new vxcf_form_install();
+   $settings = get_option( vxcf_form::$id . '_meta', [] );
+if ( ! empty( $settings['plugin_data'] ) ) {
+	$install->remove_data();
 }
